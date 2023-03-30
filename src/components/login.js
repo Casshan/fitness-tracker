7b43e0adf0bb2e260a78da5a0d24dc278416ef63
landen <landenjwilliams@gmail.com>
2023-03-30 15:46:10 -0500

    template laid out

--- src/components/login.js
+++ src/components/login.js
@@ -2,9 +2,24 @@ import React from "react";
 
 const Login = () => {
     return (
-        <>
-            <h1>Login Page</h1>
-        </>
+        <div class="container" id="loginform">
+            <br />
+            <form>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                    <label class="form-check-label" for="exampleCheck1">Remember me?</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     )
 }
 
