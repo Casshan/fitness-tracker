0f6818d09a7504e5d8e2e0baf6b24d7aa54cebf9
Cass <cass@Casss-MBP.attlocal.net>
2023-04-02 12:24:01 -0500

    Revert "connected server to db"
    This reverts commit 5ee83a66884860046d2cfa285cdeba0e44e3a5c2.

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 //importing client from db
+// const { client } = require('./db/client.js');
 
 //assinging port and making connection
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    // client.connect();
+    console.log(`Listening on PORT ${PORT}`);
+})
 
 module.exports = app;
--- db/client.js
+++ db/client.js
@@ -1,7 +1,10 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
 
-const client = new Client(connectionString);
+const client = new Pool({
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 module.exports = client;
